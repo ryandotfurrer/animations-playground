69f2233b78d53d8c6e9020685b0387a02311420c
Ryan Furrer <ryandotfurrer@gmail.com>
2026-01-16 10:19:47 -0500

    - move off turbopack to webpack due to hot reloading issues - continue working on touch target

--- app/experiments/touch-target-hack/page.tsx
+++ app/experiments/touch-target-hack/page.tsx
@@ -5,7 +5,7 @@ export default function TouchTargetHackPage() {
   return (
     <article>
       <div>
-        <header className="mb-12 text-center">
+        <header className="my-24 text-center">
           <h1>
             Touch Target Hack
           </h1>
@@ -91,6 +91,38 @@ export default function TouchTargetHackPage() {
   }
 }`}</code>
 </pre>
+        </section>
+        <section className="space-y-8">
+          <h2 >
+            What if you have multiple buttons close together?
+          </h2>
+          <div className="bg-linear-to-r from-blue-500/15 text-blue-950 dark:text-blue-50 to-transparent from-80% border-l-2 p-4 border-blue-500 flex flex-col space-y-2 text-sm"><span className='uppercase text-sm font-medium tracking-tight'>note</span><p>This section works best if you can pull this up on your phone or tablet.</p></div>
+          <p>While this is okay with a mouse or trackpad, it becomes a problem with a touch screen. All of these buttons have actually been scaled up 200% of their normal value for this demo. Below are examples at their regular size.</p>
+          <div className="mx-auto flex py-16 items-center  bg-card border rounded justify-center ">
+            <Button variant="outline" size="icon-xs" className="touch-hitbox-demo">
+              <ArrowLeftIcon className="size-3" />
+            </Button>
+            <Button variant="outline" size="icon-xs" className="touch-hitbox-demo">
+              <ArrowRightIcon className="size-3" />
+            </Button>
+            <Button variant="outline" size="icon-xs" className="touch-hitbox-demo">
+              <PlusIcon className="size-3" />
+            </Button>
+          </div>
+          <p>While you can still click these easily, it's not ideal for your mobile users which still accounts for majority of web traffic. Across all websites, <strong>mobile/touch devices account for 63–65% of all web traffic</strong>. In certain sectors, like e-commerce, that number is even higher — up to 71.8%!</p>
+          <p>If we add some gap to the containing <code>div</code>, suddenly the middle button has a much larger area to hit.</p>
+          <div className="mx-auto flex py-16 items-center gap-4 bg-card border rounded justify-center">
+            <Button variant="outline" size="icon-xs" className="touch-hitbox-demo">
+              <ArrowLeftIcon className="size-3" />
+            </Button>
+            <Button variant="outline" size="icon-xs" className="touch-hitbox-demo">
+              <ArrowRightIcon className="size-3" />
+            </Button>
+            <Button variant="outline" size="icon-xs" className="touch-hitbox-demo">
+              <PlusIcon className="size-3" />
+            </Button>
+          </div>
+          <p>As you can see, even though there is still some overlap, you can still click and tap the button you want.</p>
         </section>
       </main>
     </article>
